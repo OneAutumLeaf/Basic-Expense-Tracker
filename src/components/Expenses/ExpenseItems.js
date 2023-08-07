a56789e2384e0377fabcd5dd87dc7106e33744a9
OneAutumLeafe <ayushsingh12072000@gmaile.com>
2023-08-07 10:47:11 +0530

    new_commit

--- src/components/Expenses/ExpenseItems.js
+++ src/components/Expenses/ExpenseItems.js
@@ -1,7 +1,15 @@
 
-const ExpenseItems=(props)=>{
-
-    return(<h2>New</h2>)
-}
+import './ExpenseItems.css';
+const ExpenseItems = (props) => {
+  return (
+    <div className='expense-item'>
+      <div>March 28th 2023</div>
+      <div className='expense-item__description'>
+        <h2>Car Insurance</h2>
+        <div className='expense-item__price'>Amount</div>
+      </div>
+    </div>
+  );
+};
 
 export default ExpenseItems;
